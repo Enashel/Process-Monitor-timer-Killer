7d4a415100017c891ad495b87ed518ecad78691e
Enashel <marius.tureac2@gmail.com>
2022-08-06 15:10:05 +0300

    Initial commit

---
+++ Process monitor app/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Process_monitoring
+{
+    partial class monitoring
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(monitoring));
+            this.submit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.worktime = new System.Windows.Forms.TextBox();
+            this.pid = new System.Windows.Forms.TextBox();
+            this.mofreq = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(77, 199);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Work time (minutes)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Monitoring frequency (minutes)";
+            // 
+            // worktime
+            // 
+            this.worktime.Location = new System.Drawing.Point(63, 114);
+            this.worktime.Name = "worktime";
+            this.worktime.Size = new System.Drawing.Size(100, 20);
+            this.worktime.TabIndex = 4;
+            this.worktime.TextChanged += new System.EventHandler(this.worktime_TextChanged);
+            // 
+            // pid
+            // 
+            this.pid.Location = new System.Drawing.Point(63, 59);
+            this.pid.Name = "pid";
+            this.pid.Size = new System.Drawing.Size(100, 20);
+            this.pid.TabIndex = 5;
+            // 
+            // mofreq
+            // 
+            this.mofreq.Location = new System.Drawing.Point(63, 173);
+            this.mofreq.Name = "mofreq";
+            this.mofreq.Size = new System.Drawing.Size(100, 20);
+            this.mofreq.TabIndex = 6;
+            this.mofreq.TextChanged += new System.EventHandler(this.mofreq_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(276, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 186);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Refresh List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 9;
+   
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // monitoring
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 322);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.mofreq);
+            this.Controls.Add(this.pid);
+            this.Controls.Add(this.worktime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "monitoring";
+            this.Text = "Process Monitor";
+            this.Load += new System.EventHandler(this.monitoring_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox worktime;
+        private System.Windows.Forms.TextBox pid;
+        private System.Windows.Forms.TextBox mofreq;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Process monitor app/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+namespace Process_monitoring
+{
+
+    public partial class monitoring : Form
+    {
+        TimeSpan rtime;
+        Process[] processes;
+        Process[] pbyname;
+        string prbyname;
+        public monitoring()
+        {
+            InitializeComponent();
+        }
+
+        public void submit_Click(object sender, EventArgs e)
+        {
+            textboxcheck();
+            progrm();
+        }
+        public void worktime_TextChanged(object sender, EventArgs e)
+        {
+            float fwValue;
+            if (!float.TryParse(worktime.Text, out fwValue))
+            {
+                worktime.Text = "";
+            }
+        }
+
+        public void mofreq_TextChanged(object sender, EventArgs e)
+        {
+            float ffValue;
+            if (!float.TryParse(mofreq.Text, out ffValue))
+            {
+                mofreq.Text = "";
+            }
+        }
+
+        public void monitoring_Load(object sender, EventArgs e)
+        {
+            ListProcesses();
+            timer2.Stop();
+            timer1.Stop();
+            
+        }
+        private void ListProcesses()
+        {
+            processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                listBox1.Items.Add(p.ProcessName);
+            }
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ListProcesses();
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            pid.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program is on for: " + rtime.TotalMinutes.ToString("F") + " minutes");
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {if(timer1.Enabled)
+            {
+                timer2.Start();
+                timer2.Interval = 1000;
+                progrm();
+            }
+
+        }
+        public void textboxcheck()
+        {
+            switch (String.IsNullOrEmpty(pid.Text))
+            {
+                case true:
+                    MessageBox.Show("Please insert a process name");
+
+                    break;
+                case false:
+
+                    break;
+            }
+
+            switch (String.IsNullOrEmpty(worktime.Text))
+            {
+                case true:
+                    MessageBox.Show("Please insert process run time");
+
+                    break;
+                case false:
+
+                    break;
+            }
+            switch (String.IsNullOrEmpty(mofreq.Text))
+            {
+                case true:
+                    MessageBox.Show("Please insert app monitor frequency time");
+
+                    break;
+                case false:
+
+                    break;
+            }
+
+        }
+        public void progrm()
+        {
+            prbyname = pid.Text;
+            pbyname = Process.GetProcessesByName(prbyname);
+            if ((String.IsNullOrEmpty(pid.Text) || String.IsNullOrEmpty(worktime.Text) || String.IsNullOrEmpty(mofreq.Text)) != true)
+            {
+                if (pbyname.Length > 0)
+                {
+                    foreach (Process p in pbyname)
+                    {
+
+                        try
+                        {
+                            rtime = DateTime.Now - p.StartTime;
+                            textBox1.Text = rtime.TotalSeconds.ToString("F");
+                            float mworktime = (float.Parse(worktime.Text) * 1000) * 60;
+                            if (mworktime < (float)rtime.TotalMilliseconds)
+
+                            {
+                                p.Kill();
+                                timer2.Stop();
+                                ListProcesses();
+                            }
+                           
+                            timer1.Interval= (int.Parse(mofreq.Text) * 1000) * 60;
+                            timer1.Enabled = true;
+                            timer1.Start();
+                               
+                        }
+                        catch (Win32Exception except)
+                        {
+                            if (except.NativeErrorCode == 5)
+                                continue;
+                            throw;
+                        }
+
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "Process is not started";
+                }
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
